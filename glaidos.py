dbec4b540724d8fbdbe66a5b5675e649d4030279
Patrick Schmitt <schmitt@technikum-wien.at>
2023-07-23 22:04:46 +0200

    Updated filter again

--- glaidos.py
+++ glaidos.py
@@ -166,7 +166,7 @@ def run_glaidos():
             or text == "Thank you for watching!" or text == "Thank you for your interest." or text == "Thank you for listening. Have a great day. "
             or text == "Thank you for watching." or text == ". ." or text == "Thank you. Bye. " or text == "Bye. " or text == "It's done."
             or text == "Thank you very much." or text == "Bye-bye. " or text == "Bye. Bye. " or text == "Thanks for having me." or text == "Bye-bye."
-            or text == ". . ." or text == " . . ."):
+            or text == ". . ." or text == " . . ." or text == "Thank you so much. Bye bye."):
                 #print(f"WARNING!: Previous Input was ignored - just displayed for debugging. GOT: {text}") # enable this line if further debugging info is required
                 continue
         
